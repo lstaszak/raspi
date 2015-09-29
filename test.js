5606122b4b70a776d5a0c20cd03121a1f8898444
Lukasz Staszak <lstaszak@gmail.com>
2015-09-29 14:57:53 +0200

    test

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
-//var five = require("firebase");
-var firebase = require("johnny-five");
-//var firebaseRef = new firebase("https://beelab.firebaseio.com/test");
+var five = require("../lib/johnny-five.js");
 var board = new five.Board();
+var firebase = require("firebase");
+var firebaseRef = new firebase("https://beelab.firebaseio.com/test");
 board.on("ready", function () {
-  var relay1 = new five.Led(9);
-  //var relay2 = new five.Relay(10);
-  //relay1.off();
-  //relay2.off();
-  /*
-   this.repl.inject({
-   relay: relay1,
-   relay: relay2
-   });
-   var light = firebaseRef.child("light");
-   light.on("value", function (snapshot) {
-   console.log(snapshot.val());
-   if (snapshot.val().one == "1 on") {
-   relay1.on();
-   }
-   if (snapshot.val().one == "2 on") {
-   relay1.off();
-   }
-   if (snapshot.val().one == "1, 2 off") {
-   relay1.on();
-   relay2.on();
-   }
-   });
-   */
+  var relay1 = new five.Relay(9);
+  var relay2 = new five.Relay(10);
+  relay1.off();
+  relay2.off();
+  this.repl.inject({
+    relay: relay1,
+    relay: relay2
+  });
+  var light = firebaseRef.child("light");
+  light.on("value", function (snapshot) {
+    console.log(snapshot.val());
+    if (snapshot.val().one == "0") {
+      //relay1.on();
+    }
+    if (snapshot.val().one == "1") {
+      //relay1.off();
+    }
+    if (snapshot.val().two == "0") {
+      //relay2.on();
+    }
+    if (snapshot.val().two == "1") {
+      //relay2.off();
+    }
+  });
 });
