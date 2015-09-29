edfce4e445f9fd1da56e9a89afd5e099f0c33008
Lukasz Staszak <lstaszak@gmail.com>
2015-09-29 14:14:24 +0200

    test

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
 var five = require("johnny-five");
 var board = new five.Board();
+var firebase = require("firebase");
+var firebaseRef = new firebase("https://beelab.firebaseio.com/test");
 board.on("ready", function () {
-  var led = new five.Led(13);
-  led.blink(500);
+  var relay1 = new five.Relay(9);
+  var relay2 = new five.Relay(10);
+  relay1.off();
+  relay2.off();
+  this.repl.inject({
+    relay: relay1,
+    relay: relay2
+  });
+  var light = firebaseRef.child("light");
+  light.on("value", function (snapshot) {
+    console.log(snapshot.val());
+    if (snapshot.val().one == "1 on") {
+      relay1.on();
+    }
+    if (snapshot.val().one == "2 on") {
+      relay1.off();
+    }
+    if (snapshot.val().one == "1, 2 off") {
+      relay1.on();
+      relay2.on();
+    }
+  });
 });
